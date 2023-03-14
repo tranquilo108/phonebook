6cf6883a11b0310fbb60f0aa97322a06b9af6dfd
tranquilo108 <118503079+tranquilo108@users.noreply.github.com>
2023-03-15 02:01:35 +0700

    commit

---
+++ controller.py
@@ -0,0 +1,17 @@
+import view
+import model
+def run():
+    view.greeting()
+    while True:
+        view.menu()
+        choice = int(input())
+        match choice:
+            case 1:
+                model.find()
+            case 2:
+                model.add_contact()
+            case 3:
+                view.show_phonebook()
+            case _:
+                print('Введено неверное знаечние, попробуем снова =)')
+                view.menu()
