6c35b0bc1db7a3e7746985ad5d5fe0edf9e726e3
Kazitsyn <71144819+Kazitsyn@users.noreply.github.com>
2023-03-15 19:01:02 +0300

    new view

---
+++ view.py
@@ -0,0 +1,20 @@
+from os import name
+
+
+def greeting():
+    print('Weolcome to the phonebook program. \n')
+
+def menu():
+    print('********************************\n           Phonebook\n********************************\n\n 1. Find\n 2. Add contact\n 3. Show phonebook\n 4. Quit\n')
+
+def show_phonebook():
+    path = 'file.txt'
+    file = open(path, 'r')
+    print('\n********************************\n')
+    for line in file:
+        print(line)
+    file.close
+    print('\n********************************')
+
+def find_menu():
+    print('********************************\n           Choice\n********************************\n\n 1. Edit contact\n 2. Delete contact\n 3. Return menu\n')
